2b8afcb58f150df4e2c21cf2e2da4a81e2b54c39
ALHWARY <nnytttdh@gmail.com>
2026-08-22 22:49:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber:'201556853817', // Bot number
+  phoneNumber:'201211883781', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
