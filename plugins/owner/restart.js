e2aa3883a873842a8e687bf7c3ba7b5f94a06431
ALHWARY <nnytttdh@gmail.com>
2026-08-25 13:43:03 +0300

    Update restart.js

--- plugins/owner/restart.js
+++ plugins/owner/restart.js
@@ -1,8 +1,8 @@
 const test = async (m, { conn, bot }) => {
-  m.react("🟢")
+  m.react("🍂")
   
   conn.msgUrl(m.chat, "♡゙ Bot is restarting...", { 
-    title: "𝘗𝘰𝘮𝘯𝘪 𝘈𝘐 𝘪𝘴 𝘢 𝘞𝘩𝘢𝘵𝘴𝘈𝘱𝘱 𝘣𝘰𝘵 𝘧𝘳𝘰𝘮 𝘵𝘩𝘦 𝘝𝘐𝘐7𝘓𝘐𝘕𝘒 𝘓𝘪𝘣𝘳𝘢𝘳𝘺",
+    title: "آلبوت بيـﮯعمل رستارت دلوقتي",
     body: "𝑇𝒉𝑒 𝑏𝑜𝑡 𝑖𝑠 𝑠𝑖𝑚𝑝𝑙𝑒 𝑡𝑜 𝑚𝑜𝑑𝑖𝑓𝑦",
     img: "https://g.top4top.io/p_3700yob0b1.jpg",
     big: false 
